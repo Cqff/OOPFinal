2a690787cec115193caba3f64b55497395755290
111306047 <118511796+111306047@users.noreply.github.com>
2023-06-11 00:48:00 +0800

    Update Interface.java
    change

--- FinalProject/src/Interface.java
+++ FinalProject/src/Interface.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 interface Interface {
 	
+	public static final JFrame frame = new JFrame();
+    public static final JTable table = new JTable();
+    public static final DefaultTableModel tableModel = new DefaultTableModel();
+    
 	void createTable();
-	ArrayList<Goods> getBooksFromDatabase() ;
-	void show();
-
+	
 }
