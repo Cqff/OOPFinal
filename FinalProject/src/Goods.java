db38aca301123b80dc0b0e209408f965fb1af4d0
111306047 <118511796+111306047@users.noreply.github.com>
2023-06-15 21:35:49 +0800

    Update Goods.java

--- FinalProject/src/Goods.java
+++ FinalProject/src/Goods.java
@@ -6,13 +6,16 @@ public class Goods {
 	 private String name;
 	 private double price;
 	 private String author;
+	 private String bookType;
+	 private String bookCat;
 	
-	public Goods(int ID,String name, double price, String author) {
+	public Goods(int ID, String name, double price, String author) {
 		 this.ID = ID;
 		 this.name = name;
 		 this.price = price;
 		 this.author = author;
 	}
+	
 	
 	public int getID() {
 		return ID;
@@ -29,4 +32,28 @@ public class Goods {
 	public String getAuthor() {
 		return author;
 	}
+
+	public void setId(int ID) {
+		this.ID = ID;
+		
+	}
+
+	public void setTitle(String name) {
+		this.name = name;
+		
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+		
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+		
+	}
+	
+	public String toString() {
+        return  ID + "   "+ name +"   "+ price + "  " + author;
+    }
 }
